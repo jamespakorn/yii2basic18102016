3d37776cd5983965c3ae008e062659688ef99650
jamespakorn <jameshosxp@hotmail.com>
2016-10-18 16:23:49 +0700

    project yii2basic

---
+++ views/site/test.php
@@ -0,0 +1,24 @@
+<h1><?php echo $title;?></h1>
+<br>
+<?php
+
+echo 'This is Test view';
+echo '<hr>';
+echo 'data from controller ::'.$cat;
+?>
+<hr>
+<h3>แสดงผลแบบ Array  </h3>
+<?php
+print_r($ar);
+echo '<hr>';
+print_r($person);
+echo '<hr>';
+
+foreach ($person as $item) {
+    echo $item['fname'].' / '.$item['lname'].' / '.$item['email'].'<br>';
+}
+
+
+
+
+
